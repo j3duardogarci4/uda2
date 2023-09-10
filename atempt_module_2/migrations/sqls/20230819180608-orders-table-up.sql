0425804178e3292a71208c7faabfea4a8a2988f6
Juan E. GM <juanedo77@gmail.com>
2023-09-10 00:17:46 -0300

    Update 20230819180608-orders-table-up.sql
    detail table

--- atempt_module_2/migrations/sqls/20230819180608-orders-table-up.sql
+++ atempt_module_2/migrations/sqls/20230819180608-orders-table-up.sql
@@ -1,15 +1,24 @@
+CREATE TABLE ORDERS (ID_ORDER INT4,
+	             IDCUST INT4,
+	             STATUS_ORDER INT4,
+	             DATE date);
+	
+CONSTRAINT fk_customer
+FOREIGN KEY(idcust) 
+REFERENCES users(idlogin);
+
 /* Replace with your SQL commands */
-CREATE TABLE orders (
+CREATE TABLE orders_detail (
 	id int4 NULL,
-	idcust int4 NULL,
+	iddetail int4,
+--	idcust int4 NULL,
 	idprod int4 NULL,
 	quantity int4 NULL,
 	statusorder int4 NULL
 );
 
-CONSTRAINT fk_customer
-FOREIGN KEY(idcust) 
-   REFERENCES users(idlogin);
+
+   
 CONSTRAINT fk_product
 FOREIGN KEY(idprod) 
    REFERENCES products(id);
